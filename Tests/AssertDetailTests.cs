054e24fe9b5157d0c1609cd079ff54e778da945f
Sylvain Chamberland <gentx2@gmail.com>
2019-06-28 12:35:56 -0400

    Ajout d'un test d'égalité de deux objets complexes différents - le message d'erreur de Assert.Equal est peu utile car on ne sait pas quelles sont les différences entre les objets

---
+++ Tests/AssertDetailTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Tests
+{
+    [Trait("Assertions", "Détails")]
+    public class AssertDetailTests
+    {
+        [Fact]
+        public void EqualIndiqueDifferencesEntreDeuxObjetsComplexesInegaux()
+        {
+            var attendu = new Groupe<DepotCourrier>(
+                "C000",
+                new DepotCourrier[]
+                {
+                    new DepotCourrier("C010", "1134 1er étage"),
+                    new DepotCourrier("C011", "1134 1er étage"),
+                    new DepotCourrier("C012", "1134 1er étage"),
+                    new DepotCourrier("C013", "1134 1er étage"),
+                });
+
+            var reel = new Groupe<DepotCourrier>(
+                "C000",
+                new DepotCourrier[]
+                {
+                    new DepotCourrier("C010", "1134 1er étage"),
+                    new DepotCourrier("C011", "1134 1er étage"),
+                    new DepotCourrier("C012", "1134 1er étage"),
+                    new DepotCourrier("C014", "1134 1er étage"),
+                });
+
+            Assert.Equal(attendu, reel);
+        }
+    }
+}
