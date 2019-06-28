6704d4143e5b8f6bd2884d941a06837ff056773b
Sylvain Chamberland <gentx2@gmail.com>
2019-06-28 10:16:38 -0400

    Ajout de classes formant un graphe complexe - DepotCourrier représente un emplacement dans un immeuble - Groupe représente une façon de regrouper des éléments

---
+++ Tests/Groupe.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tests
+{
+    public class Groupe<T> : ValueObject
+    {
+        public Groupe(string nom, IEnumerable<T> elements)
+        {
+            Nom = nom ?? throw new ArgumentNullException(nameof(nom));
+            Elements = elements ?? throw new ArgumentNullException(nameof(elements));
+        }
+
+        public string Nom { get; }
+        public IEnumerable<T> Elements { get; }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Nom;
+
+            foreach (var element in Elements)
+            {
+                yield return element;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Nom}: [{string.Join("; ", Elements.Select(e => e.ToString()))}]";
+        }
+    }
+}
